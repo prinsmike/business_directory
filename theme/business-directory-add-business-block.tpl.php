170d0eb22aae7d6c277e52f8a2c5c130fcd4064b
Michael Prinsloo <mike.prinsloo@gmail.com>
2013-10-14 15:22:07 +0200

    Initial github commit.

---
+++ theme/business-directory-add-business-block.tpl.php
@@ -0,0 +1,8 @@
+<?php global $base_url; ?>
+<div id="add-business-container">
+  <div id="add-business-button">
+    <a href="<?php $base_url; ?>/node/add/business">
+      <img src="<?php print $base_url . '/' . drupal_get_path('module', 'business_directory'); ?>/images/add-business.button.horizontal.png" />
+    </a>
+  </div>
+</div>
